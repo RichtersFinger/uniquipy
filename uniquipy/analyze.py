19b5c4b6613deeed58ff1e6ff32843d5dd9de640
Steffen Richters-Finger <steffen.finger.privat@gmail.com>
2023-12-29 10:09:04 +0100

    Implement analyze-command
    * implement function to identify duplicates
    
    * improve analyze-command output

--- uniquipy/analyze.py
+++ uniquipy/analyze.py
@@ -2,22 +2,87 @@
 This module contains the definition of the analyze-command.
 """
 
+import sys
+from pathlib import Path
 import click
+from uniquipy import src
+from uniquipy.src import HASHING_ALGORITHMS as methods
+
 
 @click.command()
-@click.option("-i", "--input-directory", "input_dir",
+@click.option(
+    "-i", "--input-directory", "input_dir",
     required=True,
     type=click.Path(exists=True),
     help="path to the input directory"
 )
-@click.option("-v", "--verbose", "verbose",
+@click.option(
+    "-m", "--hash-algorithm", "hash_algorithm",
+    default=list(methods.keys())[0],
+    show_default=True,
+    type=click.Choice(
+        list(methods.keys()),
+        case_sensitive=True
+    ),
+    help="specify the hash algorithm used to identify files"
+)
+@click.option(
+    "-v", "--verbose", "verbose",
     is_flag=True,
     help="verbose output"
 )
 def analyze(
     input_dir,
+    hash_algorithm,
     verbose
 ):
     """Analyze existing directory regarding file duplicates."""
 
-    click.echo("analyzing..")
+    source = Path(input_dir)
+
+    # make sure the target is valid
+    if not source.is_dir():
+        if verbose:
+            click.echo(
+                f"Error: Invalid argument for input directory {input_dir}, directory does not exist.",
+                file=sys.stderr
+            )
+        sys.exit(1)
+
+    if verbose:
+        click.echo("analyzing..")
+
+    # find all files
+    list_of_files = [p for p in source.glob("**/*") if p.is_file()]
+
+    if verbose:
+        click.echo(f"working on a set of {len(list_of_files)} files")
+
+    is_unique, uniques = src.find_duplicates(
+        list_of_files,
+        hash_algorithm
+    )
+
+    if verbose:
+        click.echo(f"number of unique files: {len(uniques)}")
+
+    if verbose:
+        if is_unique:
+            click.echo("Summary: no duplicates found")
+        else:
+            click.echo("Summary: there are duplicates")
+
+    if verbose:
+        # list duplicates
+        click.echo("="*5 + " Details " + "="*5)
+        for files in uniques.values():
+            if len(files) > 1:
+                click.echo(f"file '{files[0]}' has duplicates at")
+                click.echo("\n".join(map(lambda x: f" * {str(x)}", files[1:])))
+    else:
+        # arrange in blocks
+        click.echo(
+            "\n\n".join(
+                "\n".join(map(str, files)) for files in uniques.values()
+            )
+        )
