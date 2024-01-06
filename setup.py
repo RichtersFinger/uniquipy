b5591903369d12e5297055083b76e9380dab51c3
RichtersFinger <steffen.finger.privat@gmail.com>
2024-01-06 10:00:31 +0100

    extend package metadata
    closes #23

--- setup.py
+++ setup.py
@@ -1,10 +1,24 @@
 from setuptools import setup
+from pathlib import Path
+
+# read contents of README
+long_description = \
+    (Path(__file__).parent / "README.md").read_text(encoding="utf8")
 
 setup(
     version="1.0.0",
     name="uniquipy",
     description="python cli-tool to find and handle file duplicates",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Steffen Richters-Finger",
+    author_email="srichters@uni-muenster.de",
+    license="MIT",
+    license_files=("LICENSE",),
+    url="https://pypi.org/project/uniquipy/",
+    project_urls={
+        "Source": "https://github.com/RichtersFinger/uniquipy"
+    },
     install_requires=[
         "click>=8.1.7,<9.0.0",
     ],
